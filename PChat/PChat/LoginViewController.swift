a246e033fe3db50576aa7b091aaefdfe16540b84
GitSzuKai <GitSzuKai@users.noreply.github.com>
2017-02-23 20:01:02 -0500

    Sign up and login done

--- PChat/PChat/LoginViewController.swift
+++ PChat/PChat/LoginViewController.swift
@@ -19,11 +19,13 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        /*
+        let alertController = UIAlertController(title: "Error", message: "Failed to signup", preferredStyle: .alert)
         
-        let alertController = UIAlertController(title: "Title", message: "Message", preferredStyle: .alert)
-        
-        let cancelAction = UIAlertAction(title: "OK", style: .cancel) { (action) in }
-        alertController.addAction(cancelAction)
+
+      //  let cancelAction = UIAlertAction(title: "OK", style: .cancel) { (action) in}
+       // alertController.addAction(cancelAction)
+
 
         let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in }
         alertController.addAction(OKAction)
@@ -31,16 +33,62 @@ class LoginViewController: UIViewController {
         present(alertController, animated: true) {
         
         }
+ */
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func onSignUp(_ sender: Any) {
+        signUp()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func presentChatView() {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let chatView = mainStoryboard.instantiateViewController(withIdentifier: "ChatViewController") 
+        present(chatView, animated: true, completion: nil)
+    }
+    
+    @IBAction func onLogin(_ sender: Any) {
+        /*
+        do{
+            try PFUser.logIn(withUsername: emailTextField.text!, password: passwordTextField.text!)
+        }
+        catch{
+            let alertController = UIAlertController(title: "Error", message: "Failed to login", preferredStyle: .alert)
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in }
+            alertController.addAction(OKAction)
+            present(alertController, animated: true) {
+                
+            }
+
+        }*/
+        
+        PFUser.logInWithUsername(inBackground: emailTextField.text!, password: passwordTextField.text!) { (response: PFUser?, error: Error?) in
+            if let error = error {
+                //error
+                print(error.localizedDescription)
+                let alertController = UIAlertController(title: "Error", message: "\(error.localizedDescription)" , preferredStyle: .alert)
+                let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in }
+                alertController.addAction(OKAction)
+                self.present(alertController, animated: true) {
+                    
+                }
+
+            } else {
+                self.presentChatView()
+            }
+        }
+        
+    }
+    
+    
     
     func signUp() {
+       
+       
         let user = PFUser()
         user.username = emailTextField.text
         user.password = passwordTextField.text
@@ -49,15 +97,23 @@ class LoginViewController: UIViewController {
         //user["phone"] = "415-392-0202"
         user.signUpInBackground { (succeded, error) in
             if let error = error {
+                 let alertController = UIAlertController(title: "Error", message: "\(error.localizedDescription)", preferredStyle: .alert)
                 print(error.localizedDescription)
+                let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in }
+                alertController.addAction(OKAction)
+                self.present(alertController, animated: true) {
+                }
+                
+            
                 // Show the errorString somewhere and let the user try again.
             } else {
                 print("sign up!!")
                 // Hooray! Let them use the app now.
                 // Go to next screen
+                self.presentChatView()
             }
         }
-        
+       
     }
     
     
