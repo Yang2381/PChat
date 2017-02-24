c16e351ff4113e176baa764e43552d9c97e8f53e
GitSzuKai <GitSzuKai@users.noreply.github.com>
2017-02-23 20:09:07 -0500

    Added ChatStoryBoard

--- PChat/PChat/ChatViewController.swift
+++ PChat/PChat/ChatViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController {
+class ChatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
